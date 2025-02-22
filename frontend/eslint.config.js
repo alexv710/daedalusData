4fa7d9fa74bf17020129dd39b02286d6d7b6f3e8
Alexander Wyss <alexander.wyss@roche.com>
2025-02-22 13:57:35 +0100

    style: :art: add ESlint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,16 @@
-// @ts-check
-import antfu from '@antfu/eslint-config'
-import nuxt from './.nuxt/eslint.config.mjs'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
 
-export default nuxt(
-  antfu(
-    {
-      unocss: true,
-      formatters: true,
-    },
-  ),
-)
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+];
